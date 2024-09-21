9729e29b3dc2997d5b3edb398ee8ab69ed67ad72
pgimenez <pablo@saffire.com>
2024-09-20 22:29:35 -0300

    Backend: Project for integration tests created. Implemented BaseRepository tests and Category repository tests using InMemory Database.

---
+++ backend/ProductCrud/tests/IntegrationTests/Repositories/CategoryRepositoryTest.cs
@@ -0,0 +1,28 @@
+﻿
+using Dex.ProductCrud.Core.Entities;
+using Dex.ProductCrud.Infrastructure.Repositories;
+using GenFu;
+
+namespace Dex.ProductCrud.IntegrationTests.Repositories
+{
+    public class CategoryRepositoryTest : BaseRepositoryTest<Category>
+    {
+        public CategoryRepositoryTest(): base()
+        {
+            _asyncRepository = new CategoryRepository(_context);
+        }
+
+        protected override Category GetEntity()
+        {
+            A.Configure<Category>()
+                .Fill(c => c.Id, 0);
+
+            return A.New<Category>();
+        }
+
+        protected override void ModifyEntity(Category entity)
+        {
+            entity.Name = "Beer";
+        }
+    }
+}
